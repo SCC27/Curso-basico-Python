491fc944dd31645a491aff535fa6a4ff1e9b5d7d
SCC27 <solcisneros962705@gmail.com>
2022-03-02 01:03:24 -0600

    Herramientas para programar

--- conversor.py
+++ conversor.py
@@ -1,7 +1,32 @@
-pesos = input("¿Cuantos pesos colombianos tienes?: ")
-pesos = float(pesos)
-valor_dolar = 3875
-dolares = pesos / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
-print("Tienes $" + dolares + "dolares")
+def conversor(tipo_pesos, valor_dolar):
+    pesos = input("¿Cuantos pesos " + tipo_pesos + " tienes?: ")
+    pesos = float(pesos)
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + "dolares")
+
+
+
+
+menu = """
+Bienvenido al conversor de monedad 🔁
+
+1 - Pesos colombianos
+2 - Pesos argentinos
+3 - Pesos mexicanos
+
+Elige una opcion:
+"""
+opcion = int(input(menu))
+
+if opcion == 1:
+    conversor("Colombianos", 3875)
+
+elif opcion == 2:
+    conversor("Argentinos", 65)
+elif opcion ==3:
+    conversor("Mexicanos", 24)
+else:
+    print("Elige una opcion correcta porfavor")
+
