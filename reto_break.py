5394d8156675759cbe0cbda6a5df375bfac9c847
SCC27 <solcisneros962705@gmail.com>
2022-03-03 23:49:06 -0600

    Bucles

---
+++ reto_break.py
@@ -0,0 +1,14 @@
+def run ():
+    LIMITE= 100
+
+    contador = 0
+    while contador < LIMITE:
+        contador= contador + 1
+        if contador == 90:
+            break
+        print(contador)
+        
+
+
+if __name__ == '__main__':
+    run()
