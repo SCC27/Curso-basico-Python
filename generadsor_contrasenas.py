d1fd9f3deb86f72e0fe421e83dadff906f663a33
SCC27 <solcisneros962705@gmail.com>
2022-03-04 00:48:01 -0600

    Estructuras de datos

---
+++ generadsor_contrasenas.py
@@ -0,0 +1,27 @@
+from lib2to3.pgen2.pgen import generate_grammar
+import random
+
+def generar_contrasena():
+    mayusculas =['A', 'B', 'C', 'D', 'E', 'F', 'G']
+    minusculas =['a', 'b', 'c', 'd', 'e', 'f', 'gG']
+    simbolos=['!', '#', '$', '&', '/', '(', ')']
+    numeros=['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+
+    caracteres = mayusculas + minusculas + simbolos + numeros
+
+    contrasena = []
+
+    for i in range(15):
+        caracter_random = random.choice(caracteres)
+        contrasena.append(caracter_random)
+
+    contrasena = "".join(contrasena)
+    return contrasena
+
+
+def run ():
+    contrasena = generar_contrasena()
+    print(' Tu nueva contraseña es '+ contrasena)
+
+if __name__ == '__main__':
+    run()
